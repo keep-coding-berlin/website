3a1a71993a657fd6a9899e8e5dd2ac8f828581de
enrroquez <93826869+enrroquez@users.noreply.github.com>
2023-11-13 02:03:40 +0100

    Initial proposal

---
+++ test.js
@@ -0,0 +1,6 @@
+.box { width:400px; height:400px; position:relative; background:#ccc;
+    .img { width:400px; height:400px; margin:0; } 
+    .options { position:absolute; top:0; left:20px; width:360px; height:40px; display:none; } 
+    .box:hover
+    .options { display:block; }
+    .button { border-radius:5px 5px 5px; background:#f0f0f0; margin:2px; width:40px; height:20px; display:inline-block; padding:2px 8px; font-size:14px; border:1px solid #d0d0d0;}
